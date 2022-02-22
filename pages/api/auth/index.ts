9eca30d6973da93ad55ab715b0148618ce773cc7
y4shiro <y4shironao@gmail.com>
2022-02-22 17:16:06 +0900

    Login API のモック実装

---
+++ pages/api/auth/index.ts
@@ -0,0 +1,24 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import TokenHelper from '../../../helpers/token';
+
+type AuthRequest = NextApiRequest & {
+  body: {
+    id: string;
+    password: string;
+  };
+};
+
+const auth = async (req: AuthRequest, res: NextApiResponse): Promise<void> => {
+  if (req.body.id && req.body.password && 0 < req.body.id.lastIndexOf('.com')) {
+    const payload = { user: req.body.id };
+
+    const token = TokenHelper.sign(payload);
+
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    res.status(200).json({ status: 'ok', token });
+  } else {
+    res.status(401).json({ message: 'Unauthorized' });
+  }
+};
+
+export default auth;

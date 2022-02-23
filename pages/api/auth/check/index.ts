12d9f1ba0b35fae97f0981a7ed3757b01d9a9da3
y4shiro <y4shironao@gmail.com>
2022-02-23 17:27:30 +0900

    Session Check API のモック 実装

---
+++ pages/api/auth/check/index.ts
@@ -0,0 +1,26 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import jwt from 'jsonwebtoken';
+import TokenHelper, { SECRET_KEY } from '../../../../helpers/token';
+
+const check = (req: NextApiRequest, res: NextApiResponse): void => {
+  let token = '';
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.split(' ')[0] === 'Bearer'
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+
+    jwt.verify(token, SECRET_KEY, (err: any, decoded: any) => {
+      if (err) {
+        res.status(401).json({ message: 'Unauthorized' });
+      } else {
+        const token = TokenHelper.sign(decoded.user);
+        res.status(200).json({ status: 'ok', token });
+      }
+    });
+  } else {
+    res.status(401).json({ message: 'Unauthorized' });
+  }
+};
+
+export default check;
